5e59d166f9f855e5ae46e1ebb916779eff0afedf
Jul Lang <jullanggit@proton.me>
2026-03-18 14:14:51 +0100

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Development shell for burn-npu";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            pkg-config
+            openssl
+            openvino
+          ];
+        };
+      }
+    );
+}
